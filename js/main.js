97a1ff3ef2c0ae36fa1a0f0a53c12e37973870b1
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-11-23 15:43:01 +0100

    Stato sviluppo: - Creato un ciclo foreach per inserire nel DOM gli oggetti dell'array;

--- js/main.js
+++ js/main.js
@@ -55,7 +55,45 @@ const posts = [
         "created": "03-05-2021"
     }
 ];
+console.log(posts);
 
-// creo il forEach per stampare gli oggetti dell'array nel DOM
+// richiamo il contenitore in cui inserire i post generati
+const ctnPost = document.getElementById("container");
 
+// creo il  ciclo forEach per stampare gli oggetti dell'array nel DOM
+posts.forEach(elem => {
+
+    let post = `<div class="post">
+                    <div class="post__header">
+                        <div class="post-meta">                    
+                            <div class="post-meta__icon">
+                                <img class="profile-pic" src=${elem.author.image} alt=${elem.author.name}>                    
+                            </div>
+                            <div class="post-meta__data">
+                                <div class="post-meta__author">${elem.author.name}</div>
+                                <div class="post-meta__time">${elem.created}</div>
+                            </div>                    
+                        </div>
+                    </div>
+                    <div class="post__text">${elem.content}</div>
+                    <div class="post__image">
+                        <img src=${elem.media} alt="Image not found...">
+                    </div>
+                    <div class="post__footer">
+                        <div class="likes js-likes">
+                            <div class="likes__cta">
+                                <a class="like-button  js-like-button" href="#" data-postid="1">
+                                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                    <span class="like-button__label">Mi Piace</span>
+                                </a>
+                            </div>
+                            <div class="likes__counter">
+                                Piace a <b id="like-counter-1" class="js-likes-counter">${elem.likes}</b> persone
+                            </div>
+                        </div> 
+                    </div>            
+                </div>`
+
+    ctnPost.innerHTML += post;
+});
 
